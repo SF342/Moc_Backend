190e836556202e2795f7adfe0a75bb8174ae6b7f
WonYus <lucy_oliv@outlook.com>
2022-03-30 00:39:35 +0700

    update db
    controller, model
    product,  productid

--- controllers/FavoriteController.js
+++ controllers/FavoriteController.js
@@ -1,22 +1,112 @@
 const Favorite = require('../model/Favorite');
+const Productid = require('../model/Productid');
+const Product = require('../model/Product');
 const User = require('../model/user')
 const { default: mongoose } = require('mongoose');
+const axios = require('axios')
 
 // create task
 const createFavorite = async (req, res) => {
 
     // get value from body
-    const { _id, product_id, product_name } = req.body
+    const { _id, product_id } = req.body
+
+    //date calculate
+    const now = Date.now();
+    const date = new Date(now)
+    console.log(date)
+    var year = date.getFullYear(), month = date.getMonth() + 1, day = date.getDate();
+    console.log(year, month, day);
+    const today = year + '-' + (month < 10 ? "0" + month : month) + '-' + (day < 10 ? "0" + day : day);
+    const getWeekAgo = () => {
+        const day = date.getDate() - 7;
+        if (day <= 0) {
+            return year + '-' + (month < 10 ? "0" + month : month) + '-' + ((30 + day) < 10 ? "0" + (30 + day) : (30 + day))
+        } else {
+            return year + '-' + (month < 10 ? "0" + month : month) + '-' + ((day) < 10 ? "0" + (day) : (day))
+        }
+    }
+    const weekAgo = getWeekAgo();
+
+    // function msToTime(duration) {
+    //     var milliseconds = Math.floor((duration % 1000) / 100),
+    //         seconds = Math.floor((duration / 1000) % 60),
+    //         minutes = Math.floor((duration / (1000 * 60)) % 60),
+    //         hours = Math.floor((duration / (1000 * 60 * 60)) % 24) + 7;
+
+    //     return { hours, minutes, seconds, milliseconds };
+    // }
+
+    let url = "https://dataapi.moc.go.th/gis-product-prices?product_id=" + product_id + "&from_date=" + weekAgo + "&to_date=" + today;
+
+    let productData = []
+    //check product id 
+    try {
+        await Productid.find({ product_id: product_id })
+            .then(async (data) => {
+                if (data.length == 0) {
+                    console.log(url);
+                    await axios.get(url)
+                        .then(async (res) => {
+                            console.log(res.data);
+                            const {
+                                product_id,
+                                product_name,
+                                product_desc_en,
+                                product_desc_th,
+                                category_name,
+                                group_name,
+                                unit,
+                                price_min_avg,
+                                price_max_avg,
+                                price_list
+                            } = res.data;
+                            await Product.create({
+                                product_id,
+                                product_name,
+                                product_desc_en,
+                                product_desc_th,
+                                category_name,
+                                group_name,
+                                unit,
+                                price_min_avg,
+                                price_max_avg,
+                                price_list
+                            });
+                        })
+                    await Productid.create({
+                        product_id,
+                        product_name,
+                    });
+
+                } else {
+                    productData.push(...data)
+                }
+            })
+    } catch (error) {
+        console.log(error.messageFormat)
+    }
+
 
     //create data on mongodb
-    const favorite = await Favorite.create({
-        id_user: new mongoose.Types.ObjectId(_id),
-        product_id,
-        product_name,
-    })
+    try {
+        const favorite = await Favorite.create({
+            user_id: new mongoose.Types.ObjectId(_id),
+            product_id,
+        })
 
-    //response back
-    res.json(favorite);
+        //response back
+        res.status(200).send({ data: favorite, message: "success" });
+    } catch (error) {
+        console.error();
+
+        //response back
+        res.status(404).send({ data: error, message: "error" });
+    }
+
+
+
+
 
 }
 
@@ -65,7 +155,7 @@ const deleteFavorite = async (req, res) => {
     const deleteFavorite = await Favorite.deleteOne({ _id: id });
 
     // response data
-    res.status(200).send({ data: deleteFavorite, word: "success" })
+    res.status(200).send({ data: deleteFavorite, message: "success" })
 }
 
 
--- model/Favorite.js
+++ model/Favorite.js
@@ -1,10 +1,9 @@
-const mongoose =  require('mongoose');
+const mongoose = require('mongoose');
 
 // Model user 
 const favoriteSchema = new mongoose.Schema({
-    id_user: {type: mongoose.SchemaTypes.ObjectId},
-    product_id: { type: String, unique: true},
-    product_name: { type: String},
+    user_id: { type: String, require: true },
+    product_id: { type: String, require: true },
 })
 
 module.exports = mongoose.model('favorite', favoriteSchema)
